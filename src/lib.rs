b8a35174f16d2e611d90181f6c7e11856a253f17
Richard Tjokroutomo <richard.tjokro2@gmail.com>
2026-09-16 13:17:49 +0800

    also add ft_get_advances (plural)
    Signed-off-by: Richard Tjokroutomo <richard.tjokro2@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1373,4 +1373,12 @@ extern "C" {
         load_flags: FT_Int32,
         padvance: *mut FT_Fixed,
     ) -> FT_Error;
+
+    pub fn FT_Get_Advances(
+        face: FT_Face,
+        start: FT_UInt,
+        count: FT_UInt,
+        load_flags: FT_Int32,
+        padvance: *mut FT_Fixed,
+    ) -> FT_Error;
 }
